f76c00e04f0a1af22cd81664e891f54f64c7f128
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-03-30 19:01:57 +0100

    added sending email on bad climate

--- monitor.py
+++ monitor.py
@@ -4,16 +4,28 @@ from temp_humid import get_temperature_and_humidity
 from microclimate_validator import validate_climate
 from db import engine, measurements
 from logger import logger
+from pi_email import send_email, test_receiver_email
+
+message = test_message = """\
+Subject: microclimate
+
+Hi, something wrong with your microclimate. It's time to something
+This message is sent from Python.
+Rasberry Pi started monitoring.
+"""
 
 while True:
     temperature, humidity = get_temperature_and_humidity()
+    is_valid = validate_climate(temperature, humidity)
     measurement = {
         'temperature': temperature,
         'humidity': humidity,
-        'is_valid': validate_climate(temperature, humidity),
+        'is_valid': is_valid,
         'datatime': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
     conn = engine.connect()
     ins = measurements.insert().values(**measurement)
-    logger.info(f'T:{temperature} C  H:{humidity}%')
+    logger.info(f'T:{temperature} C  H:{humidity}% VALID:{is_valid}')
+    if not is_valid:
+        send_email(test_receiver_email, message)
     time.sleep(5)
