d3196a43e6988c0dac31de4b4fe937af8cb268d5
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-03-30 15:33:19 +0000

    added reading info from dht11 sensor

---
+++ temp_humid/__init__.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 17
+dht11_instance = dht11.DHT11(pin=17)
+
+
+def get_temperature():
+    while True:
+        result = dht11_instance.read()
+        if result.is_valid():
+            break
+    return result.temperature
+
+def get_humidity():
+     while True:
+        result = dht11_instance.read()
+        if result.is_valid():
+            break
+    return result.humidity
+
+def get_temperature_and_humidity():
+    while True:
+        result = dht11_instance.read()
+        if result.is_valid():
+            break
+    return result.temperature, result.humidity
