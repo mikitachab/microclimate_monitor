00503b0989999063ee5cd162e4ae494487248ce1
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-04-01 18:58:17 +0200

    update tests

--- test_validator.py
+++ test_validator.py
@@ -7,3 +7,7 @@ def test_validator_return_true():
 
 def test_validator_return_false():
     assert validate_climate(temperature=27, humidity=80) == False
+
+
+def test_validator_normal_humidity_return_false():
+    assert validate_climate(temperature=27, humidity=50) == False
