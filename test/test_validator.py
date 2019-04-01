4a8bdf77f2116a47fda18211c75835a6c1360fc1
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-04-01 20:19:50 +0200

    linter corrections

--- test/test_validator.py
+++ test/test_validator.py
@@ -2,8 +2,8 @@ from microclimate_validator import validate_climate
 
 
 def test_pass_all_bad_return_false():
-    assert validate_climate(temperature=27, humidity=80) == False
+    assert validate_climate(temperature=27, humidity=80) is False
 
 
 def test_pass_all_good_return_true():
-    assert validate_climate(temperature=20, humidity=50) == True
+    assert validate_climate(temperature=20, humidity=50) is True
