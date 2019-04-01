be3322c063d504880f7bce7e08edec7f89e84571
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-04-01 20:09:35 +0200

    update tests and travis

---
+++ test/test_validator.py
@@ -0,0 +1,9 @@
+from microclimate_validator import validate_climate
+
+
+def test_pass_all_bad_return_false():
+    assert validate_climate(temperature=27, humidity=80) == False
+
+
+def test_pass_all_good_return_true():
+    assert validate_climate(temperature=20, humidity=50) == True
