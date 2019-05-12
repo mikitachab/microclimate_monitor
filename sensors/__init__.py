415e0656bd77745e9047e2161ba55f7545a545a3
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-05-12 22:54:14 +0100

    added sound sensor module

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from sensors.temp_humid import get_temperature, get_humidity
 from sensors.light import get_light
+from sensors.sound import get_sound
 from config import config
 
 sensors_map = {
     'temperature': get_temperature,
     'humidity': get_humidity,
-    'light': get_light
+    'light': get_light,
+    'is_loud': get_sound,
 }
 
 
