78c5d0e76240811c33c3df2b52b8a2d7cf1d77c0
ArfreeX <blicharz.mat@gmail.com>
2019-05-15 22:16:01 +0000

    Hotfix

--- monitor.py
+++ monitor.py
@@ -23,11 +23,11 @@ def main():
         logger.info(f'T:{temperature} C H:{humidity}% VALID:{is_valid}')
 
         if not is_valid:
-            MonitorTimer.run_more_frequently = True
+            monitor_timer.run_more_frequently = True
 
         if counter >= config['min_measurements_count']:
             counter = 1
-            MonitorTimer.run_more_frequently = False
+            monitor_timer.run_more_frequently = False
 
             avg_temp, avg_hum = mean_of_last_n_measurements(config['min_measurements_count'])
             logger.info(f'AVG_TEMP: {avg_temp}, AVG_HUM {avg_hum}')
